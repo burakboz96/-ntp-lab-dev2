b635fafde5a69432540c015f63a41978ef29a693
burak bozoğlu <burakbozoglu96@gmail.com>
2022-10-24 18:53:47 +0300

    Add files via upload

---
+++ proje 2/proje 2/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////// ödev 
+            //int sonuc = (48 / 6 / 4);
+            //int sonuc2 = (24 / 3*4);
+            //int sonuc3 = (2*2+5-1+4);
+            //int sonuc4= ((7+4)*2-1+8/2);
+            //int sonuc5 = ((5 - 1) * 2 * 1 + 7 *7/ 2);
+            //Console.WriteLine(sonuc);
+            //Console.WriteLine(sonuc2);
+            //Console.WriteLine(sonuc3);
+            //Console.WriteLine(sonuc4);
+            //Console.WriteLine(sonuc5);
+
+            //////////////////////////////// ödev
+
+            //   Console.WriteLine("İKİ SAYI GİRİNİZ= ");
+            //int giris = Convert.ToInt32(Console.ReadLine());
+
+            //int giris2=Convert.ToInt32(Console.ReadLine());
+
+            //   if (giris % giris2 == 0) 
+            //   Console.WriteLine("girilen sayılar birbirine bölünüyor");
+            //  else
+            //   Console.WriteLine("girilen değerler birbirine bölünmüyor");
+
+
+            //////////////////////////////////////////////////////////////////
+
+
+            //             string name, name2;
+            //Console.Write("Lütfen Bir Ay İsmi Giriniz = ");
+            //name = Convert.ToString(Console.ReadLine());
+            //name2 = name.ToLower();
+            //switch (name2)
+            //{
+            //      case "ocak":
+            //    Console.Write("Ocak 1. Aydır");
+            //    break;
+            //     case "şubat":
+            //      Console.Write("Şubat 2. Aydır");
+            //       break;
+            //        case "mart":
+            //   Console.Write("Mart 3. Aydır");
+            //    break;
+            //   case "Nisan":
+            //    Console.Write("Nisan 4. Aydır");
+            //   break;
+            //case "mayıs":
+            //Console.Write("Mayıs 5. Aydır");
+            //   break;
+
+            //   case "haziran":
+            // Console.Write("Haziran 6. Aydır");
+            //  break;
+
+            //case "temmuz":
+            //   Console.Write("Temmuz 7. Aydır");
+            //break;
+
+            //   case "ağustos":
+            //    Console.Write("Ağustos 8. Aydır");
+            //  break;
+
+            //  case "eylül":
+            //      Console.Write("Eylül 9. Aydır");
+            //break;
+            //case "ekim":
+            //       Console.Write("Ekim 10. Aydır");
+            //     break;
+            //   case "kasım":
+            //        Console.Write("Kasım 11. Aydır");
+            //break;
+            //   case "aralık":
+            //  Console.Write("Aralık 12. Aydır");
+            //  break;
+            //    default:   // 
+            //      Console.Write("Böyle Dir Ay Yoktur!");
+            //  break;
+            //}
+
+            //           Console.ReadKey();
+
+
+
+            ////////////////////////////////////////////////
+
+
+            //Console.Write("A Değerini Giriniz = ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //Console.Write("B Değerini Giriniz = ");
+
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.Write("C Değerini Giriniz = ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+            //double d = (b * b) - (4 * a * c); // formül b^2-4ac dir
+
+            //if (d > 0)
+            //{
+            //    double x1 = ((-b - Math.Sqrt(d)) / (2 * a));
+
+            //    double x2 = ((-b + Math.Sqrt(d)) / (2 * a));
+
+            //    Console.WriteLine("Denklemin İki Kökü Vardır = ");
+
+            //    Console.WriteLine("x1 =" + x1);
+            //    Console.WriteLine("x2 =" + x2);
+            //}
+            //else if (d == 0)
+
+            //{
+            //    double x = -b / (2 * a);
+
+            //    Console.WriteLine("Denklemin Bir Kökü Vardır = ");
+            //    Console.WriteLine("x =" + x);
+            //}
+            //else
+
+            //{
+            //    Console.Write("Sanal Kök Vardır");
+            //}
+            //Console.ReadKey();
+
+
+
+
+            //////////////////////////////////////////////////////////
+
+
+
+
+            //Console.WriteLine("BİR AÇI DEĞERİ GİRİNİZ= ");
+            //int giris = Convert.ToInt32(Console.ReadLine());
+
+            //double radyan = (giris * 3.14 / 180);
+
+            //Console.WriteLine("GİRİLEN DEĞERİN RADYANI .... "+radyan);
+
+            //double gradyan = (giris * 200 / 180);
+            //Console.WriteLine("GİRİLEN DEĞERİN GRADYANI .... " + gradyan);
+
+
+
+
+        }
+    }
+}
